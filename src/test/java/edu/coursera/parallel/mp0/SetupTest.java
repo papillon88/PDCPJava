68aae2b03c277552d2ca46116e4953f0d2e6b6ad
papillon <devopreet.ch@gmail.com>
2017-10-01 08:31:03 -0500

    Initial commit

---
+++ src/test/java/edu/coursera/parallel/mp0/SetupTest.java
@@ -0,0 +1,15 @@
+package edu.coursera.parallel.mp0;
+
+import edu.coursera.parallel.mp0.Setup;
+import junit.framework.TestCase;
+
+public class SetupTest extends TestCase {
+
+    /*
+     * A simple test case.
+     */
+    public void testSetup() {
+        final int result = Setup.setup(42);
+        assertEquals(42, result);
+    }
+}
